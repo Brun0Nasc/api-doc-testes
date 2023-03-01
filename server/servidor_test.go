f6be1875f1dda32e899a108b3dc1a0ed99ec9f37
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2023-03-01 10:18:42 -0300

    adicionando caso de teste para jogador Pedro e refatorando função ObterJogadores

--- server/servidor_test.go
+++ server/servidor_test.go
@@ -6,6 +6,13 @@ import (
 	"testing"
 )
 
+func comparaResultados(t *testing.T, recebido, esperado string){
+	t.Helper()
+	if recebido != esperado {
+		t.Errorf("recebido '%s', esperado '%s'", recebido, esperado)
+	}
+}
+
 func TestObterJogadores(t *testing.T) {
 	t.Run("retornar resultado de Maria", func(t *testing.T) {
 		requisicao, _ := http.NewRequest(http.MethodGet, "/jogadores/Maria", nil)
@@ -16,8 +23,18 @@ func TestObterJogadores(t *testing.T) {
 		recebido := resposta.Body.String()
 		esperado := "20"
 
-		if recebido != esperado {
-			t.Errorf("recebido '%s', esperado '%s'", recebido, esperado)
-		}
+		comparaResultados(t, recebido, esperado)
+	})
+
+	t.Run("retornando resultado de Pedro", func(t *testing.T) {
+		requisicao, _ := http.NewRequest(http.MethodGet, "/jogadores/Pedro", nil)
+		resposta := httptest.NewRecorder()
+
+		ServidorJogador(resposta, requisicao)
+
+		recebido := resposta.Body.String()
+		esperado := "10"
+
+		comparaResultados(t, recebido, esperado)
 	})
 }
